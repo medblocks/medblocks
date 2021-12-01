417de36852a602ed17e6f717d2326ea2a7fe01e6
Dharsan B <dharsanborn98@gmail.com>
2021-12-01 16:38:48 +0530

    Updated docker-compose version

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ echo \
 
 
 # Installing docker-compose
-  sudo curl -L "https://github.com/docker/compose/releases/download/v2.1.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/v2.1.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
   sudo chmod +x /usr/local/bin/docker-compose
 
